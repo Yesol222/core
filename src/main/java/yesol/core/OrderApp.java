1466c2b20de4e87a4491a2879da1f6ab1e862383
yesol <744348@gmail.com>
2021-02-24 14:31:38 +0900

    applicationContext

--- src/main/java/yesol/core/OrderApp.java
+++ src/main/java/yesol/core/OrderApp.java
@@ -1,5 +1,7 @@
 package yesol.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import yesol.core.member.Grade;
 import yesol.core.member.Member;
 import yesol.core.member.MemberService;
@@ -12,12 +14,14 @@ import yesol.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-//        MemberService memberService = new MemberServiceImpl();
-//        OrderService orderService = new OrderServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- src/main/java/yesol/core/order/AppConfig.java
+++ src/main/java/yesol/core/order/AppConfig.java
@@ -1,27 +1,35 @@
 package yesol.core.order;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import yesol.core.discount.DiscountPolicy;
 import yesol.core.discount.FixDiscountPolicy;
+import yesol.core.discount.RateDiscountPoicy;
 import yesol.core.member.MemberRepository;
 import yesol.core.member.MemberService;
 import yesol.core.member.MemberServiceImpl;
 import yesol.core.member.MemoryMemberRepository;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
-    private MemberRepository memberRepository() { // 인터페이스
+    @Bean
+    public MemberRepository memberRepository() { // 인터페이스
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPoicy();
     }
 }
